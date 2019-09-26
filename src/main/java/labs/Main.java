be660f4fa6ac7727b4ed4b6e9678a1369f3b7a3e
Admin <goniva.k@gmail.com>
2019-09-26 16:13:01 +0300

    My First lab

---
+++ src/main/java/labs/Main.java
@@ -0,0 +1,17 @@
+package labs;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        /*System.out.println("input your name");
+        Scanner s = new Scanner(System.in);
+        s.next();
+        String name = s.next();
+        System.out.println("your sad your name is " + name);*/
+       /* Random r = new Random();
+        System.out.println(r.nextInt());*/
+    }
+}
